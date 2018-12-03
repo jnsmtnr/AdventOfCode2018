61bb003314174f38d4052d2030ccc11685616061
János Maitner <janosmaitner@gmail.com>
2018-12-03 10:51:59 +0100

    day 2 puzzle 1 done

--- day-2/puzzle3.ts
+++ day-2/puzzle3.ts
@@ -7,5 +7,40 @@ let fileContent = fs.readFileSync('input.txt', 'utf-8');
 
 let inputArray = fileContent.split('\n');
 
-console.log(inputArray[0]);
-console.log(inputArray[inputArray.length-1]);
+let numberOfTwoLetters = 0
+let numberOfThreeLetters = 0
+
+for (let elem of inputArray) {
+
+  let letters = {}
+
+  let twoLetters = false
+  let threeLetters = false
+  
+  for (let letter of elem) {
+    
+    if (letters[letter]) {
+      letters[letter] += 1
+    } else {
+      letters[letter] = 1
+    }
+  }
+  
+  for (let key in letters) {
+    if (letters[key] === 2) {
+      twoLetters = true
+    }
+    if (letters[key] === 3) {
+      threeLetters = true
+    }
+  }
+
+  if (twoLetters) {
+    numberOfTwoLetters++
+  }
+  if (threeLetters) {
+    numberOfThreeLetters++
+  }
+}
+
+console.log(numberOfTwoLetters*numberOfThreeLetters)

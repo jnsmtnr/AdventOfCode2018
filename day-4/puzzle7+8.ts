060a82579ad7cd40967ce1eeb70fc9f358f5decd
János Maitner <janosmaitner@gmail.com>
2018-12-04 21:58:17 +0100

    found the most sleepy guard :)

--- day-4/puzzle7+8.ts
+++ day-4/puzzle7+8.ts
@@ -1,4 +1,4 @@
-export {}
+export { }
 
 'use strict'
 
@@ -9,13 +9,58 @@ let fileContent = fs.readFileSync('input.txt', 'utf-8');
 
 let inputArray = fileContent.split('\n')
 
-if (inputArray[inputArray.length-1] == '') {
+if (inputArray[inputArray.length - 1] == '') {
   inputArray.pop()
 }
 
 const dateRegEx = /[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}\:[0-9]{2}/
 
-inputArray.sort( function(a,b) {
-  return a.match(dateRegEx)[0].replace(/[-: ]/g,'') - b.match(dateRegEx)[0].replace(/[-: ]/g,'')
+let chrono = inputArray.sort(function (a, b) {
+  return a.match(dateRegEx)[0].replace(/[-: ]/g, '') - b.match(dateRegEx)[0].replace(/[-: ]/g, '')
 })
+
+/* for (let i = 0; i < 11; i++) {
+  console.log(chrono[i])
+} */
+
+let guardSleep = {}
+let currentGuard = ''
+let startSleep = 0
+let stopSleep = 0
+let sleepTime = 0
+
+for (let i = 0; i < chrono.length-1; i++) {
+  
+  if (chrono[i].match(/#[0-9]*/)) {
+    currentGuard = chrono[i].match(/#[0-9]*/)[0]
+    //console.log(currentGuard)
+  } else if (chrono[i].indexOf('asleep') != -1) {
+    startSleep = Number(chrono[i].match(/:[0-9]*/)[0].replace(':',''))
+    //console.log('start sleeping at: ' + String(startSleep))
+  } else if (chrono[i].indexOf('wakes') != -1) {
+    stopSleep = Number(chrono[i].match(/:[0-9]*/)[0].replace(':',''))
+    //console.log('stop sleeping at: ' + String(stopSleep))
+    sleepTime = stopSleep - startSleep
+    //console.log('slept for: ' + String(sleepTime))
+    if (guardSleep[currentGuard]) {
+      guardSleep[currentGuard] += sleepTime
+    } else {
+      guardSleep[currentGuard] = sleepTime
+    }
+  }
+}
+
+let maxSleep = 0
+
+for (let key in guardSleep) {
+  if (guardSleep[key] > maxSleep) {
+    maxSleep = guardSleep[key]
+  }
+}
+
+for (let key in guardSleep) {
+  if (guardSleep[key] == maxSleep) {
+    console.log(key)
+  }
+}
 

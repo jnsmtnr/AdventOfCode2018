7d6f50788c1176fa97921b740400ce689df05cc9
János Maitner <janosmaitner@gmail.com>
2018-12-10 13:34:34 +0100

    day 5 part 2 done

--- day-5/puzzle10.ts
+++ day-5/puzzle10.ts
@@ -6,3 +6,31 @@ declare function require(path: string): any;
 const fs = require('fs');
 
 let polymer = fs.readFileSync('input.txt', 'utf-8');
+
+let abc = []
+
+for (let letter of polymer) {
+  if (abc.indexOf(letter.toUpperCase()) == -1) {
+    abc.push(letter.toUpperCase())
+  }
+}
+
+abc.forEach( function(letter) {
+  let letterToChange = new RegExp(letter,'gi',)
+  let newPolymer = polymer.replace(letterToChange,'').split('')
+  let itsOn = true
+  while (itsOn) {
+    itsOn = false
+    for (let i = 0; i < newPolymer.length - 1; i++) {
+      if (
+        (newPolymer[i] === newPolymer[i + 1].toUpperCase() && newPolymer[i].toLowerCase() === newPolymer[i + 1])
+        || (newPolymer[i].toUpperCase() === newPolymer[i + 1] && newPolymer[i] === newPolymer[i + 1].toLowerCase())
+      ) {
+        newPolymer.splice(i,2)
+        itsOn = true
+        break
+      }
+    }
+  }
+  console.log(newPolymer.length)
+})

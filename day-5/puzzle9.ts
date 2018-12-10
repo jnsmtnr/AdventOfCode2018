7d23690047a01e12fb0dc0c9dcf67ceb2ddcd1cf
János Maitner <janosmaitner@gmail.com>
2018-12-10 12:42:51 +0100

    day 5 part 1 done

--- day-5/puzzle9.ts
+++ day-5/puzzle9.ts
@@ -5,6 +5,22 @@ export { }
 declare function require(path: string): any;
 const fs = require('fs');
 
-let fileContent = fs.readFileSync('input.txt', 'utf-8');
+let polymer = fs.readFileSync('input.txt', 'utf-8').split('');
 
-console.log(fileContent[0])
+let itsOn = true
+
+while (itsOn) {
+  itsOn = false
+  for (let i = 0; i < polymer.length - 1; i++) {
+    if (
+      (polymer[i] === polymer[i + 1].toUpperCase() && polymer[i].toLowerCase() === polymer[i + 1])
+      || (polymer[i].toUpperCase() === polymer[i + 1] && polymer[i] === polymer[i + 1].toLowerCase())
+    ) {
+      polymer.splice(i,2)
+      itsOn = true
+    }
+  }
+}
+
+console.log(polymer.length -1)
+console.log(polymer[polymer.length-3], polymer[polymer.length-2], polymer[polymer.length-1])

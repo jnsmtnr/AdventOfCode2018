39ad91e3c51714668f6ddac424a5879c0f04c0a2
János Maitner <janosmaitner@gmail.com>
2018-12-14 22:04:42 +0100

    day 7 part 1 done

--- day-7/puzzle13.ts
+++ day-7/puzzle13.ts
@@ -15,3 +15,36 @@ let manualFiltered = manual.map(elem => {
   return [splitted[1], splitted[7]]
 })
 
+let stepsNotDone = []
+manualFiltered.forEach( elem => {
+  if (stepsNotDone.indexOf(elem[0]) == -1) {
+    stepsNotDone.push(elem[0])
+  }
+  if (stepsNotDone.indexOf(elem[1]) == -1) {
+    stepsNotDone.push(elem[1])
+  }
+})
+
+stepsNotDone.sort()
+
+let stepsDone = []
+
+while (stepsNotDone.length > 0) {
+  for (let i=0; i<stepsNotDone.length; i++) {
+    let canItBeDone = true
+    manualFiltered.forEach( elem => {
+      if ( stepsNotDone[i] == elem[1] ) {
+        if ( stepsDone.indexOf(elem[0]) == -1) {
+          canItBeDone = false
+        }
+      }
+    })
+    if (canItBeDone) {
+      stepsDone.push(stepsNotDone[i])
+      stepsNotDone.splice(i, 1)
+      break
+    }
+  }
+}
+
+console.log(stepsDone.join(''))

96c13d803d55de3fb477c15e63850e658e5a4463
János Maitner <janosmaitner@gmail.com>
2018-12-14 23:20:13 +0100

    day 7 part 2 done

--- day-7/puzzle14.ts
+++ day-7/puzzle14.ts
@@ -26,3 +26,54 @@ manualFiltered.forEach( elem => {
 })
 
 stepsNotDone.sort()
+
+let stepsDone = []
+let stepsWorkingOn = []
+let stepsDoable = []
+let time = 0
+let timeLeft = {}
+
+stepsNotDone.forEach( (value, index) => {
+  timeLeft[value] = 61 + index
+})
+
+while (stepsDone.length < 26) {
+  time++
+  
+  // megnézem, melyik lépéseket lehet elkezdeni
+  for (let i=0; i<stepsNotDone.length; i++) {
+    let canItBeDone = true
+    manualFiltered.forEach (elem => {
+      if (stepsNotDone[i] == elem[1]) {
+        if (stepsDone.indexOf(elem[0]) == -1 ) {
+          canItBeDone = false
+        }
+      }
+    })
+    if (canItBeDone) {
+      stepsDoable.push(stepsNotDone[i])
+      stepsNotDone.splice(i,1)
+    }
+  }
+
+  // megnézem, hogy tudok-e további lépésen dolgozni
+  while (stepsWorkingOn.length < 5 && stepsDoable.length > 0) {
+    stepsWorkingOn.push(stepsDoable.shift())
+  }
+
+  // dolgozok a lépéseken
+  stepsWorkingOn.forEach( elem => {
+    timeLeft[elem]--
+  })
+
+  // megnézem, befejeztem-e egy lépést
+  for (let i=stepsWorkingOn.length-1; i >= 0; i--) {
+    if (timeLeft[stepsWorkingOn[i]] == 0) {
+      stepsDone.push(stepsWorkingOn[i])
+      stepsWorkingOn.splice(i,1)
+    }
+  }
+
+  console.log(stepsNotDone.join(''), '|' , stepsWorkingOn.join(''), '|' , stepsDoable.join(''), '|', stepsDone.join(''))
+}
+console.log(time)

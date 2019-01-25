33dcdb53e246bec70add588cdcbfcadaaf8caf6a
János Maitner <janosmaitner@gmail.com>
2019-01-25 21:00:43 +0100

    day 10 part 1 & part 2 done

--- day-10/puzzle19.js
+++ day-10/puzzle19.js
@@ -1,10 +1,3 @@
-const fs = require('fs')
-
-let input = fs.readFileSync('input.txt', 'utf-8')
-
-let regex = /-{0,1}\d+/g
-let rawData = input.match(regex)
-
 class Pixel {
   constructor(x,y,vx,vy) {
     this.x = Number(x),
@@ -19,13 +12,39 @@ class Pixel {
   }
 }
 
-let data = []
+const c = document.getElementById("myCanvas");
+const ctx = c.getContext("2d");
 
-for (let i = 0; i <= rawData.length - 4; i += 4) {
-  let pixel = new Pixel(rawData[i],rawData[i+1],rawData[i+2],rawData[i+3])
-  data.push(pixel)
+function drawPixel(x,y) {
+  let newX = x + 100
+  let newY = -y + 450
+  ctx.fillRect(newX,newY,1,1);
 }
 
-console.log(data[0])
-data[0].move()
-console.log(data[0])
+let time = document.getElementById('time')
+time.addEventListener("change", drawPoints)
+
+function drawPoints() {
+  let input = document.getElementById('rawData').innerText
+  let regex = /-{0,1}\d+/g
+  let rawData = input.match(regex)
+  let data = []
+  for (let i = 0; i <= rawData.length - 4; i += 4) {
+    let pixel = new Pixel(rawData[i],rawData[i+1],rawData[i+2],rawData[i+3])
+    data.push(pixel)
+  }
+  
+  c.width = c.width
+  let newTime = Number(document.getElementById('time').value)
+  for (let i=1; i<=newTime; i++) {
+    data.forEach(point => {
+      point.move()
+    })
+  }
+  data.forEach(point => {
+    drawPixel(point.x,point.y)
+  })
+  console.log(data[0],newTime)
+
+}
+
